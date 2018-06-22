05ab4f472447739f2e77612287db0608b285c1c3
Owen Klumb <owen.klumb@gmail.com>
2018-06-22 15:24:58 +0900

    fixed typings to not throw error

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,5 @@
 export interface _VueStripeCheckout {
-  install: function;
+  install: (Vue: any, options?: any) => void
 }
 export declare const VueStripeCheckout: _VueStripeCheckout;
 export default VueStripeCheckout;
