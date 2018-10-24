a5ca9d207959309200fa941bd0989c264e7a8053
Joff Tiquez <jofftiquez@gmail.com>
2018-10-24 09:51:48 +0800

    Added test

---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+const VueStripeCheckout = require('../dist');
+
+describe('VueStripeCheckout', () => {
+  test('should be an object', () => {
+    expect(typeof VueStripeCheckout).toBe('object');
+  });
+});
