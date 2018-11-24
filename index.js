f28006a03e611393d138d1eb6bef1b973a29fbd7
ABuffSeagull <reecevanatta@gmail.com>
2018-11-24 17:21:17 -0500

    Lots o' changes
    - Make the key required to be passed in if not already
    		set globally
    - Use a render function so you don't need the template compiler
    - Make the div not show up, to not mess with page layout
    - Set the Stripe defaults on all the props to
    		supress console warnings
    - Add the script in mounted so it has time to load
    - Configure it with onload so we don't have to
    		guess with setTimeout
    - Make sure the script only gets added once
    - Make sure the iframe gets removed on configure,
    		so it doesn't pile up iframes
    - Have the no key error be in Promise, so that you
    		can only set it through props if you want to
    - As billingAddress is required when shippingAddress is used,
    		just force it so the developer doesn't have to
    		worry about it
    - Move jest into devDependencies
    - Add rollup to dependencies

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import VueStripeCheckout from './dist';
+import Vue from 'vue';
+import App from './App.vue';
+import VueStripeCheckout from './src/index.js';
 
-export default VueStripeCheckout;
+Vue.use(VueStripeCheckout, 'pk_test_INH6o8QUdJyZM1TuGKs5PIsT');
+// Vue.use(VueStripeCheckout);
+
+new Vue({
+	el: '#app',
+	render: h => h(App),
+});
