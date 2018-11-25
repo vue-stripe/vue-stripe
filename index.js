690dc6964077b480b43340792cfe5c387b2caef7
ABuffSeagull <reecevanatta@gmail.com>
2018-11-25 13:57:50 -0500

    Fix index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueStripeCheckout from './src/index.js';
+import VueStripeCheckout from './dist';
 
-Vue.use(VueStripeCheckout, 'pk_test_INH6o8QUdJyZM1TuGKs5PIsT');
-// Vue.use(VueStripeCheckout);
-
-new Vue({
-	el: '#app',
-	render: h => h(App),
-});
+export default VueStripeCheckout;
