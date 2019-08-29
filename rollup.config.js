4514a32acd212d8be5b96b437d30af6fa6b4c473
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-08-30 01:18:09 +0800

    chore: create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import vue from 'rollup-plugin-vue';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    name: 'vue-stripe-checkout',
+    file: 'dist/index.js',
+    format: 'cjs'
+  },
+  plugins: [
+    commonjs(),
+    vue(),
+  ]
+};
