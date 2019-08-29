e8cbddb0e0772ce0bced7b71bd36cc96b9d400c3
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-08-30 02:49:07 +0800

    refactor: add new props

--- src/Checkout.vue
+++ src/Checkout.vue
@@ -32,10 +32,40 @@ export default {
       default: window.location.href
     },
     submitType: {
+      type: String,
+      default: 'auto',
+      validator (value) {
+        let supportedValues = ['auto', 'book', 'donate', 'pay'];
+        return supportedValues.includes(value);
+      }
+    },
+    billingAddressCollection: {
+      type: String,
+      default: 'auto',
+      validator (value) {
+        let supportedValues = ['required', 'auto'];
+        return supportedValues.includes(value);
+      }
+    },
+    clientReferenceId: {
+      type: String,
+    },
+    customerEmail: {
       type: String
+    },
+    sessionId: {
+      type: String
+    },
+    locale: {
+      type: String,
+      default: 'auto',
+      validator (value) {
+        let supportedValues = ['auto', 'da', 'de', 'en', 'es', 'fi', 'fr', 'it', 'ja', 'nb', 'nl', 'pl', 'pt', 'sv', 'zh'];
+        return supportedValues.includes(value);
+      }
     }
   },
-  mounted () {
+  created () {
     const script = document.createElement('script');
     script.id = '_stripe-redirect-to-checkout';
     script.src = 'https://js.stripe.com/v3';
@@ -47,6 +77,9 @@ export default {
     },
     stripe () {
       return Stripe(this.key);
+    },
+    stripeElements () {
+      return this.stripe.elements();
     }
   },
   methods: {
@@ -54,11 +87,35 @@ export default {
       try {
         this.$emit('loading', true);
         this.stripe.redirectToCheckout({
-          items: this.items,
-          successUrl: this.successUrl,
+          billingAddressCollection: this.billingAddressCollection,
           cancelUrl: this.cancelUrl,
-          submitType: this.submitType 
+          clientReferenceId: this.clientReferenceId,
+          customerEmail: this.customerEmail,
+          items: this.items,
+          locale: this.locale,
+          sessionId: this.sessionId,
+          submitType: this.submitType ,
+          successUrl: this.successUrl,
         });
+      } catch (e) {
+        this.$emit('error', e);
+      } finally {
+        this.$emit('loading', false);
+      }
+    },
+    /** TODO: Create stripe elements
+     * @param {Object} cardElement
+     * @param {Object} data - Refer to https://stripe.com/docs/api/payment_methods/create
+     */
+    async createPaymentMethod (cardElement, data) {
+      try {
+        this.$emit('loading', true);
+        const { paymentMethod } = await this.stripe.createPaymentMethod(
+          'card',
+          cardElement,
+          data
+        );
+        this.$emit('paymentMethod', paymentMethod);
       } catch (e) {
         this.$emit('error', e);
       } finally {
