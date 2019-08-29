377249fbc64f462465c8e5edba9674c7ac4ffb25
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-08-30 01:18:22 +0800

    refactor: new cjs build

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,6 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
-
-var __vue_normalize__ = _interopDefault(require('vue-runtime-helpers/dist/normalize-component.js'));
-
 //
 //
 //
@@ -23,9 +19,8 @@ var script = {
      */
     publishableKey: {
       type: String,
-      required: true
+      required: true,
     },
-
     /**
      * @type {SKUItem} - Stripe's SKU item.
      */
@@ -34,39 +29,39 @@ var script = {
     },
     successUrl: {
       type: String,
-      "default": window.location.href
+      default: window.location.href
     },
     cancelUrl: {
       type: String,
-      "default": window.location.href
+      default: window.location.href
     },
     submitType: {
       type: String
     }
   },
-  mounted: function mounted() {
-    var script = document.createElement('script');
+  mounted () {
+    const script = document.createElement('script');
     script.id = '_stripe-redirect-to-checkout';
     script.src = 'https://js.stripe.com/v3';
     document.querySelector('head').append(script);
   },
   computed: {
-    key: function key() {
+    key () {
       return this.publishableKey;
     },
-    stripe: function stripe() {
+    stripe () {
       return Stripe(this.key);
     }
   },
   methods: {
-    redirectToCheckout: function redirectToCheckout() {
+    redirectToCheckout () {
       try {
         this.$emit('loading', true);
         this.stripe.redirectToCheckout({
           items: this.items,
           successUrl: this.successUrl,
           cancelUrl: this.cancelUrl,
-          submitType: this.submitType
+          submitType: this.submitType 
         });
       } catch (e) {
         this.$emit('error', e);
@@ -77,44 +72,131 @@ var script = {
   }
 };
 
+function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
+/* server only */
+, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+  if (typeof shadowMode !== 'boolean') {
+    createInjectorSSR = createInjector;
+    createInjector = shadowMode;
+    shadowMode = false;
+  } // Vue.extend constructor export interop.
+
+
+  var options = typeof script === 'function' ? script.options : script; // render functions
+
+  if (template && template.render) {
+    options.render = template.render;
+    options.staticRenderFns = template.staticRenderFns;
+    options._compiled = true; // functional template
+
+    if (isFunctionalTemplate) {
+      options.functional = true;
+    }
+  } // scopedId
+
+
+  if (scopeId) {
+    options._scopeId = scopeId;
+  }
+
+  var hook;
+
+  if (moduleIdentifier) {
+    // server build
+    hook = function hook(context) {
+      // 2.3 injection
+      context = context || // cached call
+      this.$vnode && this.$vnode.ssrContext || // stateful
+      this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
+      // 2.2 with runInNewContext: true
+
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__;
+      } // inject component styles
+
+
+      if (style) {
+        style.call(this, createInjectorSSR(context));
+      } // register component module identifier for async chunk inference
+
+
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier);
+      }
+    }; // used by ssr in case component is cached and beforeCreate
+    // never gets called
+
+
+    options._ssrRegister = hook;
+  } else if (style) {
+    hook = shadowMode ? function () {
+      style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
+    } : function (context) {
+      style.call(this, createInjector(context));
+    };
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // register for functional component in vue file
+      var originalRender = options.render;
+
+      options.render = function renderWithStyleInjection(h, context) {
+        hook.call(context);
+        return originalRender(h, context);
+      };
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate;
+      options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+    }
+  }
+
+  return script;
+}
+
+var normalizeComponent_1 = normalizeComponent;
+
 /* script */
-var __vue_script__ = script;
+const __vue_script__ = script;
+
 /* template */
-
-var __vue_render__ = function __vue_render__() {
+var __vue_render__ = function() {
   var _vm = this;
-
   var _h = _vm.$createElement;
-
   var _c = _vm._self._c || _h;
-
-  return _c('div', [_vm._t("checkout-button")], 2);
+  return _c("div", [_vm._t("checkout-button")], 2)
 };
-
 var __vue_staticRenderFns__ = [];
-/* style */
+__vue_render__._withStripped = true;
 
-var __vue_inject_styles__ = undefined;
-/* scoped */
+  /* style */
+  const __vue_inject_styles__ = undefined;
+  /* scoped */
+  const __vue_scope_id__ = undefined;
+  /* module identifier */
+  const __vue_module_identifier__ = undefined;
+  /* functional template */
+  const __vue_is_functional_template__ = false;
+  /* style inject */
+  
+  /* style inject SSR */
+  
 
-var __vue_scope_id__ = undefined;
-/* module identifier */
+  
+  var VueStripeCheckout = normalizeComponent_1(
+    { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
+    __vue_inject_styles__,
+    __vue_script__,
+    __vue_scope_id__,
+    __vue_is_functional_template__,
+    __vue_module_identifier__,
+    undefined,
+    undefined
+  );
 
-var __vue_module_identifier__ = undefined;
-/* functional template */
-
-var __vue_is_functional_template__ = false;
-/* style inject */
-
-/* style inject SSR */
-
-var VueStripeCheckout = __vue_normalize__({
-  render: __vue_render__,
-  staticRenderFns: __vue_staticRenderFns__
-}, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, undefined, undefined);
-
-var Plugin = {
-  install: function install(Vue, options) {
+const Plugin = {
+  install (Vue, options) {
     Vue.component('vue-stripe-checkout', VueStripeCheckout);
   }
 };
