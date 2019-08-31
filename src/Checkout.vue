ecc7f1035ba9624184472d0ed49b136803da1e66
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-09-01 01:02:09 +0800

    refactor: update Checkout component

--- src/Checkout.vue
+++ src/Checkout.vue
@@ -5,21 +5,13 @@
 </template>
 
 <script>
-/**
- * @typedef SKUItem
- */
+import { 
+  SUPPORTED_LANGS,
+  SUPPORTED_SUBMIT_TYPES,
+  BILLING_ADDRESS_COLLECTION_TYPES
+} from './contants';
 export default {
   props: {
-    /**
-     * @type {string} - Stripe's publishable key, from Stripe dashboard.
-     */
-    publishableKey: {
-      type: String,
-      required: true,
-    },
-    /**
-     * @type {SKUItem} - Stripe's SKU item.
-     */
     items: {
       type: Array
     },
@@ -34,18 +26,12 @@ export default {
     submitType: {
       type: String,
       default: 'auto',
-      validator (value) {
-        let supportedValues = ['auto', 'book', 'donate', 'pay'];
-        return supportedValues.includes(value);
-      }
+      validator: (value) => SUPPORTED_SUBMIT_TYPES.includes(value)
     },
     billingAddressCollection: {
       type: String,
       default: 'auto',
-      validator (value) {
-        let supportedValues = ['required', 'auto'];
-        return supportedValues.includes(value);
-      }
+      validator: (value) => BILLING_ADDRESS_COLLECTION_TYPES.includes(value)
     },
     clientReferenceId: {
       type: String,
@@ -59,34 +45,14 @@ export default {
     locale: {
       type: String,
       default: 'auto',
-      validator (value) {
-        let supportedValues = ['auto', 'da', 'de', 'en', 'es', 'fi', 'fr', 'it', 'ja', 'nb', 'nl', 'pl', 'pt', 'sv', 'zh'];
-        return supportedValues.includes(value);
-      }
-    }
-  },
-  created () {
-    const script = document.createElement('script');
-    script.id = '_stripe-redirect-to-checkout';
-    script.src = 'https://js.stripe.com/v3';
-    document.querySelector('head').append(script);
-  },
-  computed: {
-    key () {
-      return this.publishableKey;
-    },
-    stripe () {
-      return Stripe(this.key);
-    },
-    stripeElements () {
-      return this.stripe.elements();
+      validator: (value) => SUPPORTED_LANGS.includes(value)
     }
   },
   methods: {
     redirectToCheckout () {
       try {
         this.$emit('loading', true);
-        this.stripe.redirectToCheckout({
+        this.$stripe.redirectToCheckout({
           billingAddressCollection: this.billingAddressCollection,
           cancelUrl: this.cancelUrl,
           clientReferenceId: this.clientReferenceId,
@@ -94,29 +60,11 @@ export default {
           items: this.items,
           locale: this.locale,
           sessionId: this.sessionId,
-          submitType: this.submitType ,
+          submitType: this.submitType,
           successUrl: this.successUrl,
         });
       } catch (e) {
-        this.$emit('error', e);
-      } finally {
-        this.$emit('loading', false);
-      }
-    },
-    /** TODO: Create stripe elements
-     * @param {Object} cardElement
-     * @param {Object} data - Refer to https://stripe.com/docs/api/payment_methods/create
-     */
-    async createPaymentMethod (cardElement, data) {
-      try {
-        this.$emit('loading', true);
-        const { paymentMethod } = await this.stripe.createPaymentMethod(
-          'card',
-          cardElement,
-          data
-        );
-        this.$emit('paymentMethod', paymentMethod);
-      } catch (e) {
+        console.error(e);
         this.$emit('error', e);
       } finally {
         this.$emit('loading', false);
