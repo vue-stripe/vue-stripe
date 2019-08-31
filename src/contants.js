a4bf7fbd2fafe3f5bbb63919d3fba67cfd70ea00
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-09-01 01:02:33 +0800

    feat: create contants

---
+++ src/contants.js
@@ -0,0 +1,29 @@
+export const SUPPORTED_LANGS = [
+  'auto',
+  'da',
+  'de',
+  'en',
+  'es',
+  'fi',
+  'fr',
+  'it',
+  'ja',
+  'nb',
+  'nl',
+  'pl',
+  'pt',
+  'sv',
+  'zh'
+];
+
+export const SUPPORTED_SUBMIT_TYPES = [
+  'auto', 
+  'book', 
+  'donate', 
+  'pay'
+];
+
+export const BILLING_ADDRESS_COLLECTION_TYPES = [
+  'required', 
+  'auto'
+];
