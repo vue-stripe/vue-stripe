bb437c33b1effcdfd5a60c8afff789e7b364186c
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-15 21:14:15 +0800

    feat: create loader of stripe checkout script

---
+++ src/load-checkout.js
@@ -0,0 +1,9 @@
+import { STRIPE_CHECKOUT_URL } from './constants';
+
+export const loadStripeCheckout = (pk, version = 'v3', callback) => {
+  let e = document.createElement('script');
+  e.src = `${STRIPE_CHECKOUT_URL}/${version}`;
+  e.type='text/javascript';
+  document.getElementsByTagName('head')[0].appendChild(e);
+  e.addEventListener('load', callback);
+}
