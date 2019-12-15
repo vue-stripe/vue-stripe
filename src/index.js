5271c950c1c72b5eebd14782073622bf50dd177c
Jofferson R. Tiquez <jofftiquez@gmail.com>
2019-12-15 21:15:19 +0800

    refactor: update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
-import VueStripeCheckout from './Checkout.vue';
-import VueStripeElements from './Elements.vue';
+import StripeCheckout from './Checkout.vue';
+import StripeElements from './Elements.vue';
 
-const Plugin = {
-  install (Vue, opts) {
-    if (!opts || !opts.publishableKey) {
-      console.warn('Vue Stripe Checkout Error: Publishable key is required.');
-      return;
-    }
-
-    if (window.Stripe) {
-      Vue.prototype.$stripe = Stripe(opts && opts.publishableKey);
-    } else {
-      document.querySelector('#stripe-js').addEventListener('load', () => {
-        Vue.prototype.$stripe = Stripe(opts && opts.publishableKey);
-      });
-    }
-    
-    Vue.component('vue-stripe-checkout', VueStripeCheckout);
-    Vue.component('vue-stripe-elements', VueStripeElements);
-  }
-};
-
-export { VueStripeCheckout, VueStripeElements };
-export default Plugin;
+export { StripeCheckout, StripeElements };
