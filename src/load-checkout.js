4e74c9d12b9a6fcc0c00b7d24d469fe379caff9e
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-07-14 07:57:41 +0800

    refactor: check if stripe has already been loaded
    Don't load stripe if true
    
    Closes #123

--- src/load-checkout.js
+++ src/load-checkout.js
@@ -1,6 +1,10 @@
 import { STRIPE_CHECKOUT_URL } from './constants';
 
 export const loadStripeCheckout = (pk, version = 'v3', callback) => {
+  if (window.Stripe) {
+    callback();
+    return;
+  }
   let e = document.createElement('script');
   e.src = `${STRIPE_CHECKOUT_URL}/${version}`;
   e.type='text/javascript';
