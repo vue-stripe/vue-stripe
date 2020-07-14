9a4793088d826c395f5aa886c6077f34d9adbb71
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-07-14 08:51:05 +0800

    refactor: update checkout
    - update props
    - add proper validations
    - improve code in setting up redirect to checkout

--- src/Checkout.vue
+++ src/Checkout.vue
@@ -17,54 +17,59 @@ export default {
   props: {
     pk: {
       type: String,
-      required: true
+      required: true,
     },
-    mode:{
+    mode: {
       type: String,
-      default: undefined
+      default: null,
+      validator: value => ['payment', 'subscription'].includes(value),
     },
     lineItems: {
       type: Array,
-      default: undefined
+      default: null,
     },
     items: {
       type: Array,
-      default: undefined
+      default: null,
     },
     successUrl: {
       type: String,
-      default: window.location.href
+      default: window.location.href,
     },
     cancelUrl: {
       type: String,
-      default: window.location.href
+      default: window.location.href,
     },
     submitType: {
       type: String,
-      validator: (value) => SUPPORTED_SUBMIT_TYPES.includes(value)
+      validator: (value) => SUPPORTED_SUBMIT_TYPES.includes(value),
     },
     billingAddressCollection: {
       type: String,
       default: 'auto',
-      validator: (value) => BILLING_ADDRESS_COLLECTION_TYPES.includes(value)
+      validator: (value) => BILLING_ADDRESS_COLLECTION_TYPES.includes(value),
     },
     clientReferenceId: {
       type: String,
-      default: undefined
+      default: null,
     },
     customerEmail: {
       type: String,
-      default: undefined
+      default: null,
     },
     sessionId: {
       type: String,
-      default: undefined
+      default: null,
     },
     locale: {
       type: String,
       default: 'auto',
-      validator: (value) => SUPPORTED_LANGS.includes(value)
-    }
+      validator: value => SUPPORTED_LANGS.includes(value),
+    },
+    shippingAddressCollection: {
+      type: Object,
+      validator: value => value.hasOwnProperty('allowedCountries'),
+    },
   },
   methods: {
     redirectToCheckout () {
@@ -72,54 +77,34 @@ export default {
       loadStripeCheckout(this.pk, 'v3', () => {
         try {
           let stripe = window.Stripe(this.pk);
-          if (!this.sessionId) {
-            if(this.mode){
-              // new price request
-              // sample
-              // mode: 'payment'
-              // lineItems: [
-              //   {
-              //     price: 'price_xxxx',
-              //     quantity: 1
-              //   }
-              // ]
-              stripe.redirectToCheckout({
-                billingAddressCollection: this.billingAddressCollection,
-                cancelUrl: this.cancelUrl,
-                clientReferenceId: this.clientReferenceId,
-                customerEmail: this.customerEmail,
-                lineItems: this.lineItems,
-                locale: this.locale,
-                mode: this.mode,
-                submitType: this.submitType,
-                successUrl: this.successUrl,
-              });
-            } else {
-              // old sku request
-              // sample
-              // items: [
-              //   {
-              //     type: 'sku',
-              //     parent: 'sku_xxxx',
-              //     quantity: 2
-              //   }
-              // ]
-              stripe.redirectToCheckout({
-                billingAddressCollection: this.billingAddressCollection,
-                cancelUrl: this.cancelUrl,
-                clientReferenceId: this.clientReferenceId,
-                customerEmail: this.customerEmail,
-                items: this.items,
-                locale: this.locale,
-                submitType: this.submitType,
-                successUrl: this.successUrl,
-              });
-            }
-          } else {
+
+          if (this.sessionId) {
             stripe.redirectToCheckout({
               sessionId: this.sessionId
             });
+            return;
           }
+
+          if (this.lineItems.length && !this.mode) {
+            console.error(`Error: Property 'mode' is required when using 'lineItems'. See https://stripe.com/docs/js/checkout/redirect_to_checkout#stripe_checkout_redirect_to_checkout-options-mode`);
+            return;
+          }
+
+          const options = {
+            billingAddressCollection: this.billingAddressCollection,
+            cancelUrl: this.cancelUrl,
+            clientReferenceId: this.clientReferenceId,
+            customerEmail: this.customerEmail,
+            items: this.items,
+            lineItems: this.lineItems,
+            locale: this.locale,
+            mode: this.mode,
+            shippingAddressCollection: this.shippingAddressCollection,
+            submitType: this.submitType,
+            successUrl: this.successUrl,
+          };
+
+          stripe.redirectToCheckout(options);
         } catch (e) {
           console.error(e);
           this.$emit('error', e);
