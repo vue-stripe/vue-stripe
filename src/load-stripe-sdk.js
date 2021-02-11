bad76db6b008a61f61018691e12b3cfbd74460a7
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-02-11 21:49:54 +0800

    refactor: make the first param object to accomodate new option:
    - disableAdvancedFraudDetection: Boolean

--- src/load-stripe-sdk.js
+++ src/load-stripe-sdk.js
@@ -1,12 +1,14 @@
 import { STRIPE_JS_SDK_URL } from './constants';
 
-export const loadStripeSdk = (version = 'v3', callback) => {
+export const loadStripeSdk = ({ version = 'v3', disableAdvancedFraudDetection }, callback) => {
   if (window.Stripe) {
     callback();
     return;
   }
+  let url = `${STRIPE_JS_SDK_URL}/${version}`;
+  if (disableAdvancedFraudDetection) url += `${url}?advancedFraudSignals=false`;
   const e = document.createElement('script');
-  e.src = `${STRIPE_JS_SDK_URL}/${version}`;
+  e.src = url;
   e.type = 'text/javascript';
   document.getElementsByTagName('head')[0].appendChild(e);
   e.addEventListener('load', callback);
