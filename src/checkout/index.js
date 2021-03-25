33a7824ddddbafa2b93c9361abc47d0ee876dd76
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-03-25 14:58:12 +0800

    refactor: replace native loadStripeSdk with loadStripe from @stripe/stripe-js

--- src/checkout/index.js
+++ src/checkout/index.js
@@ -1,4 +1,4 @@
-import { loadStripeSdk } from '../load-stripe-sdk';
+import { loadStripe } from '@stripe/stripe-js/dist/pure.esm.js';
 import CoercePropsMixin from 'vue-coerce-props';
 import props from './props';
 export default {
@@ -8,49 +8,48 @@ export default {
     return element;
   },
   methods: {
-    redirectToCheckout () {
-      this.$emit('loading', true);
-      loadStripeSdk({
-        version: 'v3',
-        disableAdvancedFraudDetection: this.disableAdvancedFraudDetection,
-      }, () => {
-        try {
-          const stripe = window.Stripe(this.pk);
+    async redirectToCheckout () {
+      try {
+        console.warn('new implementation');
+        this.$emit('loading', true);
 
-          if (this.sessionId) {
-            stripe.redirectToCheckout({
-              sessionId: this.sessionId,
-            });
-            return;
-          }
+        if (this.disableAdvancedFraudDetection) loadStripe.setLoadParameters({ advancedFraudSignals: false });
 
-          if (this.lineItems && this.lineItems.length && !this.mode) {
-            console.error('Error: Property \'mode\' is required when using \'lineItems\'. See https://stripe.com/docs/js/checkout/redirect_to_checkout#stripe_checkout_redirect_to_checkout-options-mode');
-            return;
-          }
+        const stripe = await loadStripe(this.pk);
 
-          const options = {
-            billingAddressCollection: this.billingAddressCollection,
-            cancelUrl: this.cancelUrl,
-            clientReferenceId: this.clientReferenceId,
-            customerEmail: this.customerEmail,
-            items: this.items,
-            lineItems: this.lineItems,
-            locale: this.$coerced.locale,
-            mode: this.mode,
-            shippingAddressCollection: this.shippingAddressCollection,
-            submitType: this.submitType,
-            successUrl: this.successUrl,
-          };
-
-          stripe.redirectToCheckout(options);
-        } catch (e) {
-          console.error(e);
-          this.$emit('error', e);
-        } finally {
-          this.$emit('loading', false);
+        if (this.sessionId) {
+          stripe.redirectToCheckout({
+            sessionId: this.sessionId,
+          });
+          return;
         }
-      });
+
+        if (this.lineItems && this.lineItems.length && !this.mode) {
+          console.error('Error: Property \'mode\' is required when using \'lineItems\'. See https://stripe.com/docs/js/checkout/redirect_to_checkout#stripe_checkout_redirect_to_checkout-options-mode');
+          return;
+        }
+
+        const options = {
+          billingAddressCollection: this.billingAddressCollection,
+          cancelUrl: this.cancelUrl,
+          clientReferenceId: this.clientReferenceId,
+          customerEmail: this.customerEmail,
+          items: this.items,
+          lineItems: this.lineItems,
+          locale: this.$coerced.locale,
+          mode: this.mode,
+          shippingAddressCollection: this.shippingAddressCollection,
+          submitType: this.submitType,
+          successUrl: this.successUrl,
+        };
+
+        stripe.redirectToCheckout(options);
+      } catch (e) {
+        console.error(e);
+        this.$emit('error', e);
+      } finally {
+        this.$emit('loading', false);
+      }
     },
   },
 };
