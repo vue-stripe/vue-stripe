fecd55aad17b2c8f54455fef9fad077b29ad3e70
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-04-26 10:35:53 +0800

    refactor: export new component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import StripePlugin from './stripe';
 import StripeCheckout from './checkout';
 import StripeElementCard from './elements/Card.vue';
+import StripeElementPaymentRequestButton from './elements/PaymentRequestButton.vue';
 import StripeElementsPlugin from './elements';
 
 export {
   StripePlugin,
   StripeCheckout,
   StripeElementCard,
+  StripeElementPaymentRequestButton,
   StripeElementsPlugin,
 };
