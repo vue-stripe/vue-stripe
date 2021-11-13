6cec2ec9f26ec0e7838228592fce9f882850695e
Charles Watkins <charlesw@stripe.com>
2021-11-12 19:28:15 -0800

    Add Payment Element support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import StripePlugin from './stripe';
 import StripeCheckout from './checkout';
 import StripeElementCard from './elements/Card.vue';
+import StripePaymentElement from './elements/Payment.vue';
 import StripeElementsPlugin from './elements';
 
 export {
   StripePlugin,
   StripeCheckout,
   StripeElementCard,
+  StripePaymentElement,
   StripeElementsPlugin,
 };
