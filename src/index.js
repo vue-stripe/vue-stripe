fbb16e151c4302464c896c886904b5fd29e5acc2
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-11-21 20:08:48 +0800

    refactor: rename import to follow pattern

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import StripePlugin from './stripe';
 import StripeCheckout from './checkout';
 import StripeElementCard from './elements/Card.vue';
-import StripePaymentElement from './elements/Payment.vue';
+import StripeElementPayment from './elements/Payment.vue';
 import StripeElementsPlugin from './elements';
 
 export {
   StripePlugin,
   StripeCheckout,
   StripeElementCard,
-  StripePaymentElement,
+  StripeElementPayment,
   StripeElementsPlugin,
 };
