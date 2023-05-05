7c727c458ea5610b92205f0c5f7da0dd2f744988
MahoMuri <paolo.santos_12@yahoo.com.ph>
2023-05-05 08:47:44 +0800

    feat: added functionality for useElements

--- src/stripe/elements.js
+++ src/stripe/elements.js
@@ -1,9 +1,22 @@
 import { ref } from 'vue';
+import { hasElementIntent } from '../utilities';
 export const useElements = () => {
   const elements = ref(null);
 
   function createElements (stripe, options) {
-    return stripe.elements(options);
+    if (!stripe?.value) {
+      throw new Error('Error: Stripe instance is not initiated!');
+    }
+
+    if (!hasElementIntent(options)) {
+      throw new Error('Error: Properties \'mode\', \'amount\', and \'currency\' is required!');
+    } else if (!options.clientSecret) {
+      throw new Error('Error: \'clientsSecret\' is required!');
+    }
+
+    const elementsInstance = stripe.elements(options);
+    elements.value = elementsInstance;
+    return elementsInstance;
   }
 
   return {
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -2,3 +2,7 @@ export const isSecureHost = (testMode) => {
   if (testMode) return true;
   return window.location.hostname === 'localhost' || window.location.protocol === 'https:';
 };
+
+export const hasElementIntent = (options) => {
+  return options.mode && options.currency && options.amount;
+};
